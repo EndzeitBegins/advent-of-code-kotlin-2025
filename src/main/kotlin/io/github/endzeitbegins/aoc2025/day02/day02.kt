248f171d213422e60323886e47e51d87eaf8330b
EndzeitBegins <16666115+EndzeitBegins@users.noreply.github.com>
2025-12-02 13:14:59 +0100

    feat: add solution for day 2 part 2

--- src/main/kotlin/io/github/endzeitbegins/aoc2025/day02/day02.kt
+++ src/main/kotlin/io/github/endzeitbegins/aoc2025/day02/day02.kt
@@ -7,12 +7,51 @@ import kotlin.math.log10
 
 private fun part1(input: String): Long =
     readRanges(input)
-        .sumOf { range -> range.countInvalidIDs() }
+        .sumOf { range ->
+            val startID = range.firstNotNullOfOrNull { id ->
+                "$id".takeIf { it.length % 2 == 0 }
+            }
+            if (startID == null)
+                return@sumOf 0L
+
+            var sum = 0L
+
+            var number = startID.take(startID.length / 2).toLong()
+            var derivedNumber = "$number$number".toLong()
+
+            while (derivedNumber <= range.last) {
+                if (derivedNumber in range) {
+                    sum += derivedNumber
+                }
+                number += 1
+                derivedNumber = "$number$number".toLong()
+            }
+
+            return@sumOf sum
+        }
 
 
-private fun part2(input: String): Int {
-    return input.length
-}
+private fun part2(input: String): Long =
+    readRanges(input)
+        .sumOf { range ->
+            val foundIDs = mutableSetOf<Long>()
+
+            var number = 1
+            do {
+                var repeats = 2
+                var derivedNumber = "$number".repeat(repeats).toLong()
+                while (derivedNumber <= range.last) {
+                    if (derivedNumber in range) {
+                        foundIDs += derivedNumber
+                    }
+                    repeats += 1
+                    derivedNumber = "$number".repeat(repeats).toLong()
+                }
+                number += 1
+            } while (repeats > 2)
+
+            return@sumOf foundIDs.sum()
+        }
 
 private fun readRanges(input: String): List<LongRange> =
     input.split(",")
@@ -21,30 +60,6 @@ private fun readRanges(input: String): List<LongRange> =
             start.toLong()..end.toLong()
         }
 
-private fun LongRange.countInvalidIDs(): Long {
-    val range = this
-
-    val startID = range.firstNotNullOfOrNull { id ->
-        "$id".takeIf { it.length % 2 == 0 }
-    }
-    if (startID == null)
-        return 0L
-
-    var sum = 0L
-
-    var number = startID.take(startID.length / 2).toLong()
-    var derivedNumber = "$number$number".toLong()
-
-    while (derivedNumber <= range.last) {
-        if (derivedNumber in range) {
-            sum += derivedNumber
-        }
-        number += 1
-        derivedNumber = "$number$number".toLong()
-    }
-
-    return sum
-}
 
 fun main() {
     val testInput = readInput("day02/test-input.txt")
@@ -55,6 +70,6 @@ fun main() {
     println(part1(input))
 
     // part 2
-    // checkSolution(part2(testInput), 1)
-    // println(part2(input))
+    checkSolution(part2(testInput), 4174379265L)
+    println(part2(input))
 }
