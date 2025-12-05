7d14f551d0448dcc5a8fb77912f7c3434bb75f24
EndzeitBegins <16666115+EndzeitBegins@users.noreply.github.com>
2025-12-05 13:26:46 +0100

    feat: add solution for day 5 part 2

--- src/main/kotlin/io/github/endzeitbegins/aoc2025/day05/day05.kt
+++ src/main/kotlin/io/github/endzeitbegins/aoc2025/day05/day05.kt
@@ -6,24 +6,33 @@ import io.github.endzeitbegins.aoc2025.readInput
 private fun part1(input: String): Int {
     val (rangeDeclarations, idDeclarations) = input.split("\n\n")
 
-    val ranges = rangeDeclarations
-        .lines()
-        .map { rangeDeclaration ->
-            val (start, end) = rangeDeclaration.split("-")
-
-            start.toLong()..end.toLong()
-        }
-    val ids = idDeclarations
-        .lines()
-        .map(String::toLong)
+    val ranges = parseRanges(rangeDeclarations)
+    val ids = parseIDs(idDeclarations)
 
     return ids.count { id ->
         ranges.any { range -> id in range }
     }
 }
 
-private fun part2(input: String): Int {
-    return input.length
+private fun part2(input: String): Long {
+    var highestFreshID = -1L
+    var freshCount = 0L
+
+    val (rangeDeclarations, _) = input.split("\n\n")
+    val ranges = parseRanges(rangeDeclarations)
+        .sortedBy(LongRange::first)
+
+    for (range in ranges) {
+        val lower = maxOf(range.first, highestFreshID + 1)
+        val upper = range.last
+
+        if (upper > highestFreshID) {
+            freshCount += upper - lower + 1
+            highestFreshID = upper
+        }
+    }
+
+    return freshCount
 }
 
 fun main() {
@@ -35,6 +44,20 @@ fun main() {
     println(part1(input))
 
     // part 2
-    // checkSolution(part2(testInput), 1)
-    // println(part2(input))
+     checkSolution(part2(testInput), 14)
+     println(part2(input))
 }
+
+private fun parseRanges(rangeDeclarations: String): List<LongRange> =
+    rangeDeclarations
+        .lines()
+        .map { rangeDeclaration ->
+            val (start, end) = rangeDeclaration.split("-")
+
+            start.toLong()..end.toLong()
+        }
+
+private fun parseIDs(idDeclarations: String): List<Long> =
+    idDeclarations
+        .lines()
+        .map(String::toLong)
